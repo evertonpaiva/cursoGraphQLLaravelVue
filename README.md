15aa52093e4cd389b2b8576c6da57830e0a0a32d
Everton Paiva <evertonpaiva@gmail.com>
2019-09-19 06:56:17 -0300

    Versão inicial do repositório.

---
+++ README.md
@@ -0,0 +1,3 @@
+# GraphQL w/ Laravel & Vue - Lighthouse Package Backend 
+
+https://www.youtube.com/watch?v=YryCjrZ7Vqs&list=PLEhEHUEU3x5qsA5JnRzhgOghrH9Vqz4cg
---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#WEB
+WEB_CONTAINER_NAME=booksql-laravel
+WEB_LOCAL_DIR=~/git/github/cursoGraphQLLaravelVue/booksql-laravel
+
+echo -e "\nRemovendo containers, caso existam"
+docker-compose down
+
+# Testando se já existe arquivo de configuração local
+if [ ! -f "$WEB_LOCAL_DIR/.env" ]; then
+    echo -e "Criando arquivo de configuração local .env "
+    cp "$WEB_LOCAL_DIR"/.env.dev "$WEB_LOCAL_DIR"/.env
+fi
+
+echo -e "\nCorrigindo permissao nas pastas"
+
+echo -e "\nAlterando grupo e proprietario da pasta do app"
+sudo chown $USER:www-data -R $WEB_LOCAL_DIR
+
+echo -e "\nAdicionando permissao de escrita para o grupo na pasta storage"
+sudo chmod o+w -R $WEB_LOCAL_DIR/storage
+sudo chmod g+w -R $WEB_LOCAL_DIR/storage
+
+if [ -d "$WEB_LOCAL_DIR/bootstrap/cache" ]; then
+    echo -e "\nAdicionando permissao de escrita para na pasta do bootstrap"
+    sudo chmod g+w -R $WEB_LOCAL_DIR/bootstrap/cache
+fi
+
+echo -e "\nConstruindo a imagem localmente"
+docker-compose build
+
+echo -e "\nInstalando dependências localmente"
+docker run -i --rm -v booksql-laravel:/app -e APP_ENV=dev $WEB_CONTAINER_NAME composer self-update
+docker run -i --rm -v booksql-laravel:/app -e APP_ENV=dev $WEB_CONTAINER_NAME composer install
+
+echo -e "\nIniciando container"
+docker-compose up -d
+
+echo -e "\nAcompanhe a instalacao em: docker logs -f $WEB_CONTAINER_NAME"
+echo -e "\nQuando necessario logar no container: docker exec -it $WEB_CONTAINER_NAME bash"
