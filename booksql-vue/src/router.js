cd5e25ae28838f7de99f50481d5895883f099401
Everton Paiva <evertonpaiva@gmail.com>
2019-09-20 09:41:46 -0300

    Adicionando um novo livro.

--- booksql-vue/src/router.js
+++ booksql-vue/src/router.js
@@ -22,9 +22,14 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
+      path: '/books/add',
+      name: 'addBook',
+      component: () => import('./views/AddBook.vue')
+    },
+    {
       path: '/books/:id',
       name: 'book',
-      component: () => import(/* webpackChunkName: "about" */ './views/Book.vue')
-    }
+      component: () => import('./views/Book.vue')
+    },
   ]
 })
